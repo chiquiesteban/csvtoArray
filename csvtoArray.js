90a68500918f2cd9cf995b90ee8e9e7ce32c4960
Chiqui <chiquiesteban@gmail.com>
2013-05-09 15:27:05 -0300

    Update csvtoArray.js
    Fix to avoid problem with commas in quoted sentences.

--- csvtoArray.js
+++ csvtoArray.js
@@ -1,33 +1,62 @@
+
 $(document).ready(function() {
     $.ajax({
         type: "GET",
-        url: "https://docs.google.com/spreadsheet/pub?key=0AkK7wQrGC5KhdFV6WXk1M3A3TDB1a1l4dmFjSWstYnc&output=csv",
+		url: "https://docs.google.com/spreadsheet/pub?key=0AkK7wQrGC5KhdFV6WXk1M3A3TDB1a1l4dmFjSWstYnc&output=csv",
         dataType: "text",
-        success: function(data) {processData(data);},
-     });   
+        success: function(data) {CSVToArray(data);},
+     });	 
 	 
 });
 
-function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-	  var lines = [];
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
+function CSVToArray( strData, strDelimiter ){
+  strDelimiter = (strDelimiter || ",");
+  var objPattern = new RegExp(
+    (
+      "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+      "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+      "([^\"\\" + strDelimiter + "\\r\\n]*))"
+    ),
+    "gi"
+  );
 
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                /*tarr.push(headers[j]+":"+data[j]);*/
-				tarr.push(data[j]);
-            }
-            lines.push(tarr);
-        }
+  var arrData = [[]];
+  var arrMatches = null;
+
+  while (arrMatches = objPattern.exec( strData )){
+
+    var strMatchedDelimiter = arrMatches[ 1 ];
+    if (
+      strMatchedDelimiter.length &&
+      (strMatchedDelimiter != strDelimiter)
+    ){
+      arrData.push( [] );
+
     }
-	
-	useCSV(lines);
+    if (arrMatches[ 2 ]){
+      var strMatchedValue = arrMatches[ 2 ].replace(
+        new RegExp( "\"\"", "g" ),
+        "\""
+      );
+
+    } else {
+
+      var strMatchedValue = arrMatches[ 3 ];
+
+    }
+    arrData[ arrData.length - 1 ].push( strMatchedValue );
+  }
+  
+  var finalArray =[]
+  for (var i in arrData) {
+	  if (i!=0) {
+	  finalArray.push(arrData)
+	  }
+  }
+ 	
+	useCSV(finalArray);
 }
 
 function useCSV(data) {
-	console.log("Do whatever you want in this function using the data from the CSV", data, data[1], data[0][1])
+	console.log("Do whatever you want in this function using the data from the CSV", data, data[0], data[0][1])
 }
